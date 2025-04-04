e35eca04c1fc7259bc913d86ab246c7841806a9c
MylenaRoberta <my.roberta.9@gmail.com>
2025-04-03 22:19:56 -0300

    refactor(gdc-tcga): centralize project settings in a new config directory

---
+++ config/constants.py
@@ -0,0 +1,6 @@
+# File with supplementary tables (1-4) of the TCGA-BRCA paper
+BRCA_PAPER_FILE = 'brca-paper-supplementary-tables-1-to-4.xls'
+
+# List of Genomic Data Commons (GDC) API projects of interest
+GDC_API_PROJECTS = ['TCGA-BRCA']
+TOTAL_OF_PROJECTS = len(GDC_API_PROJECTS)
